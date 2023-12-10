9c458d17cd2c272ed27dca8717a68d440fd9830e
Frieda Fry <friedah.fry@gmail.com>
2023-12-10 01:42:25 -0500

    made login page be the default page and created a 'test' text editor for side-by-side comparison

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { Outlet } from 'react-router-dom';
+import Login from './Login';
+import ResetPassword from './ResetPassword';
+import NewPassword from './NewPassword';
 import './App.css';
 import './style.css';
-import { Outlet } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-
-// React Bootstrap Configuration
-import "bootstrap/dist/css/bootstrap.min.css"
 
 const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+  });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div className="align-center bg-primary pt-3">
-        <Outlet />
-      </div>
-    </ApolloProvider>
-  );
-}
+    return (
+      <ApolloProvider client={client}>
+        <div className="align-center bg-primary pt-3">
+          <Outlet />
+        </div>
+      </ApolloProvider>
+    );
+  }
+
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/reset-password" element={<ResetPassword />} />
+    <Route path="/new-password/:token" element={<NewPassword />} />
+    <Route path="/" element={<App />} />
+    <Route path="*" element={<Navigate to="/login" />} />
+  </Routes>
+);
 
 export default App;

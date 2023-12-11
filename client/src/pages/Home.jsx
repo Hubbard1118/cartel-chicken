815bcb8891b4df53f0259307dfa637e9f70c608f
Yasin Hubbard <yasin.hubbard@gmail.com>
2023-12-11 18:57:33 -0500

    main style sheet updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_MATCHUPS } from '../utils/queries';
 import PeriodicTable from './PeriodicTable';
+import '../style.css'
 
 
 const Home = () => {
@@ -50,16 +51,12 @@ const Home = () => {
         </nav>
 
 
-      <div className="title">Welcome to Chem Chicken!</div>
-        <div className="periodic-table-wrapper">
-          <div className="periodic-table-title container text-center">
+      <div className="title">Welcome to Chem Chicken!
             <div className="row">
               <div className="col">Column</div>
               <div className="col col-6">Column</div>
               <div className="col">Column</div>
             </div>
-          </div>
-
       </div>
 
       <PeriodicTable />

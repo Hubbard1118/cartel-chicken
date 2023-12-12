5d62d4f92d924c8a1068c3a301dc2d6a26f3588f
Yasin Hubbard <yasin.hubbard@gmail.com>
2023-12-12 10:23:46 -0500

    updated PT, query complete

--- client/src/utils/periodictable.js
+++ client/src/utils/periodictable.js
@@ -1,6 +1,17 @@
+const { ELEMENT_QUERY } = require("./queries");
 
 
-elementClick = (element) => {
+async function elementInfo() {
+    try {
+      // Use the Apollo Client to send the GraphQL query
+      const { data } = await client.query({
+        query: ELEMENT_QUERY,
+      });
+  
+      console.log('Fetched data:', data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
 
-
-}
+  

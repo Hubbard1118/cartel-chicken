d8c999a51574f10f5b338b374bfd0847944aa5aa
Yasin Hubbard <yasin.hubbard@gmail.com>
2023-12-12 10:41:05 -0500

    Outlet type error fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import { Outlet } from 'react-router-dom';
 import './App.css';
 import './style.css';
 import { Outlet } from 'react-router-dom';

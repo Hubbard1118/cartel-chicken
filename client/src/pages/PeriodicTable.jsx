517464b65d267b1b291df5702b45e982162a9898
Yasin Hubbard <yasin.hubbard@gmail.com>
2023-12-12 20:40:18 -0500

    card with element information completed

--- client/src/pages/PeriodicTable.jsx
+++ client/src/pages/PeriodicTable.jsx
@@ -1,47 +1,57 @@
 import "./periodicTable.css";
 import element_data from "../PeriodicTableJSON.json";
-import { useQuery } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client";
 import { GET_ONE_ELEMENT } from "../utils/queries";
 import { useState } from "react";
+import Card from "./Card";
 
 const PeriodicTable = () => {
-  const [elementData, setElementData]= useState('')
-
-  const  {data}  = useQuery(GET_ONE_ELEMENT, {
-    variables: { name: elementData },
-    pollInterval: 500
-});
-
-
-  console.log("Fetched data:", data);
-
+  const  [element, {loading: dataLoading, error, data}]  = useLazyQuery(GET_ONE_ELEMENT);
+  const [selectedElement, setSelectedElement] = useState('');
+  
+  const handleButtonClick = async (elementName) => {
+      console.log(elementName);
+       await element({
+          variables: {
+              name: elementName
+            }
+        });
+        setSelectedElement(elementName);
+    };
+    if (dataLoading) return <p>Loading...</p>;
 
   return (
     <>
-    <div className="periodic-table-wrapper">
-      {element_data.elements.map((element) => (
-        <button
-          onClick={() => setElementData(element.name)}
-          className="buttons"
-          key={element.name}
-          style={{
-            gridColumn: element.xpos,
-            gridRow: element.ypos,
-          }}
-        >
-          {element.symbol}
-        </button>
-      ))}
-    </div>
-
-    <div className="card ">
-        <img></img>
-        <div className="card-body">
-            <h5 className="card-title"></h5>
-            <p className="card-text">Click below to save!</p>
-            <a href="/profile" className="btn btn-primary">Go somewhere</a>
+        <div className="periodic-table-wrapper">
+        {element_data.elements.map((element) => (
+            <button 
+            onClick={() => handleButtonClick(element.name)}
+            className="buttons"
+            key={element.name}
+            style={{
+                gridColumn: element.xpos,
+                gridRow: element.ypos,
+            }}
+            >
+            {element.symbol}
+            </button>
+        ))}
         </div>
-    </div>
+
+        {data && (
+        <div className="card " style={{
+            'width': '30%'}}>
+            <div className="card-body">
+                <h5 className="card-title">{data.element.name} - {data.element.symbol}</h5>
+                <p className="card-text">Number: {data.element.atomicNumber}</p>
+                <p className="card-text">Mass: {data.element.atomicNumber}</p>
+                <p className="card-text">{data.element.atomicNumber}</p>
+                <a href="/profile" className="btn btn-primary">Add to your saved Elements!</a>
+            </div>
+        </div>   
+        )}
+
+        
     </>
   
 )};
